0c0ca68b6aade0c50bdd93c2e1df111a54b9011f
MinjeeBanzali <minjeebanzali@gmail.com>
2025-07-19 01:05:06 +0800

    define formatDate

--- puppeteer-report-export/server.js
+++ puppeteer-report-export/server.js
@@ -135,6 +135,17 @@ app.get('/generate-report', async (req, res) => {
             return row;
         });
         
+        const formatDate = (dateObj) => {
+            return dateObj.toLocaleString('en-US', {
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit',
+                hour12: true
+            });
+        };
         // Sort rows by date in column F (index 5), latest first
         rows = [rows[0], rows[1], ...rows.slice(2)
             .map(row => {
